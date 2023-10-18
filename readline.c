a8eb289b01a2a3eb6446b265df13b26363fcca31
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-10-18 07:07:07 -0700

    readline

--- readline.c
+++ readline.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 char *read_line(void)
 {
-    char *line = NULL;
-    size_t length = 0;
-    ssize_t x;
+char *line = NULL;
+size_t length = 0;
+ssize_t x;
     if (isatty(STDOUT_FILENO))
     {
         write(STDOUT_FILENO "$ ", 2);
