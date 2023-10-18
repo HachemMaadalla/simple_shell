552aeff6f64731c825a52365ef95d5a7bb57e0b0
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-10-18 08:18:14 -0700

    strings.c

--- strings.c
+++ strings.c
@@ -1,32 +1,83 @@
 #include "shell.h"
-
-int _strlen(char *str)
+char *_strdup (const char *str)
 {
-	int len = 0;
-
-	while (str[len] != '\0')
-	{
-	len++;
-	}
-
-	return len;
+    char *ptr;
+    int i;
+    int len = 0;
+    if (str == NULL)
+    {
+        return(NULL);
+    }
+    while (*str != '\0')
+    {
+        len++;
+        str++;
+    }
+    str = str - len;
+    ptr = malloc(sizeof(char) * (len + 1));
+    if (ptr == NULL)
+    {
+        return(NULL);
+    }
+    for (i = 0; i <= len; i++)
+    {
+        ptr[i] = str[i];
+    }
+    return(ptr);
 }
 
-char *_strdup(char *str)
+int _strcmp(char *s1, char *s2)
 {
-	int len = _strlen(str) + 1;
-	char *dup = malloc(len);
-	int i=0;
-	if (dup == NULL)
-	{
-	perror("strdup");
-	exit(EXIT_FAILURE);
-	}
+    int cmp;
+    cmp = (int)*s1 - (int)*s2;
+    while (*s1)
+    {
+        if (*s1 != *s2)
+        {
+            break;
+        }
+        s1++;
+        s2++;
+        cmp = (int)*s1 - (int)*s2;
+    }
+    return(cmp);
+}
 
-	for (i = 0; i < len; i++)
-	{
-	dup[i] = str[i];
-	}
+int _strlen(char *s)
+{
+    int len = 0;
+    while (s[len])
+    {
+        len++;
+    }
+    return (len);
+}
 
-	return dup;
+char *_strcat(char *dest, char *src)
+{
+    char *p = dest;
+    while(*p)
+    {
+        p++;
+    }
+    while(*src)
+    {
+        *p = *src;
+        p++;
+        src++;
+    }
+    *p = *src;
+    return(dest);
+}
+
+char *_strcpy(char *dest, char *src)
+{
+    int i=0;
+    while(src[i])
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i]='\0';
+    return(dest);
 }
